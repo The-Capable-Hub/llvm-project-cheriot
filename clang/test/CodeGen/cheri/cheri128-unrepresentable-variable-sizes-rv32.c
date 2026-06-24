51ba2b410a509e6ff6feb00f8db7a356f1bbf6fa
Zmicier Prybysh <zprybysh@baylibre.com>
2026-06-24 18:40:19 +0200

    Port test/CodeGen/Mips/cheri/cheri128-unrepresentable-variable-sizes.c
    This test checks CHERI bounds-representability padding for global
    variables: a global whose size isn't precisely representable at the
    target's capability precision gets its section over-aligned and
    tail-padded. Checked end-to-end through clang at the IR, asm, and ELF
    section/symbol levels, over `data/tdata/bss/tbss/common` and
    default/hidden/protected visibility.
    
    The padding is emitted by target-independent AsmPrinter/MC code, so
    it runs on every CHERI backend. I wasn't able to find existing RISC-V
    test coverage for global-variable padding (the only related test,
    cheri128-unrepresentable-stack-sizes.ll, uses the stack mechanism).
    
    Ported to clang/test/CodeGen/cheri/ (the llc-driven CHERI-Generic
    harness can't run the clang frontend), splitting the test in two
    for RV32 and RV64. CHECK lines are regenerated from RISC-V output.

---
+++ clang/test/CodeGen/cheri/cheri128-unrepresentable-variable-sizes-rv32.c
@@ -0,0 +1,303 @@
+// REQUIRES: clang
+// TODO: convert to IR test (but that requires filling in the full @llvm.used array which is annoying
+// RUN: %riscv32_cheri_purecap_clang -ffunction-sections -fdata-sections %s -S -o - -emit-llvm | FileCheck %s -check-prefix IR
+// FIXME: update test for -fno-common
+// RUN: %riscv32_cheri_purecap_clang -ffunction-sections -fdata-sections %s -S -o %t.s -fcommon
+// RUN: FileCheck %s -check-prefix ASM -input-file=%t.s
+// RUN: %riscv32_cheri_purecap_clang -ffunction-sections -fdata-sections %s -c -o - -fcommon | llvm-readelf --sections --symbols - | FileCheck %s -check-prefix SECTIONS
+// Check that we can compile the generated assembly and it matches the direct emission
+// RUN: %riscv32_cheri_purecap_clang %t.s -c -o - | llvm-readelf --sections --symbols - | FileCheck %s -check-prefix SECTIONS
+
+// Check that all data sections in the ELF file are appropriately aligned and the size has been rounded up:
+// SECTIONS-LABEL: Name Type Address Off Size ES Flg Lk Inf Al
+// SECTIONS-NEXT: NULL 00000000 {{[0-9a-f]+}} 000000 00 0 0 0
+// SECTIONS-NEXT: .strtab STRTAB 00000000
+// SECTIONS-NEXT: .text PROGBITS 00000000 {{[0-9a-f]+}} 000000 00 AX 0 0 2
+// SECTIONS-NEXT: .data.char128 PROGBITS 00000000 {{[0-9a-f]+}} 000080 00 WA 0 0 16
+// SECTIONS-NEXT: .data.char4096 PROGBITS 00000000 {{[0-9a-f]+}} 001000 00 WA 0 0 512
+// SECTIONS-NEXT: .data.char65535 PROGBITS 00000000 {{[0-9a-f]+}} 010000 00 WA 0 0 8192
+// SECTIONS-NEXT: .data.char65537 PROGBITS 00000000 {{[0-9a-f]+}} 012000 00 WA 0 0 8192
+// SECTIONS-NEXT: .data.char139267 PROGBITS 00000000 {{[0-9a-f]+}} 024000 00 WA 0 0 16384
+// SECTIONS-NEXT: .tdata.thread_char128 PROGBITS 00000000 {{[0-9a-f]+}} 000080 00 WAT 0 0 16
+// SECTIONS-NEXT: .tdata.thread_char4096 PROGBITS 00000000 {{[0-9a-f]+}} 001000 00 WAT 0 0 512
+// SECTIONS-NEXT: .tdata.thread_char65535 PROGBITS 00000000 {{[0-9a-f]+}} 010000 00 WAT 0 0 8192
+// SECTIONS-NEXT: .tdata.thread_char65537 PROGBITS 00000000 {{[0-9a-f]+}} 012000 00 WAT 0 0 8192
+// SECTIONS-NEXT: .tdata.thread_char139267 PROGBITS 00000000 {{[0-9a-f]+}} 024000 00 WAT 0 0 16384
+// SECTIONS-NEXT: .bss.zero128 NOBITS 00000000 {{[0-9a-f]+}} 000080 00 WA 0 0 16
+// SECTIONS-NEXT: .bss.zero4096 NOBITS 00000000 {{[0-9a-f]+}} 001000 00 WA 0 0 512
+// SECTIONS-NEXT: .bss.zero65535 NOBITS 00000000 {{[0-9a-f]+}} 010000 00 WA 0 0 8192
+// SECTIONS-NEXT: .bss.zero65537 NOBITS 00000000 {{[0-9a-f]+}} 012000 00 WA 0 0 8192
+// SECTIONS-NEXT: .bss.zero139267 NOBITS 00000000 {{[0-9a-f]+}} 024000 00 WA 0 0 16384
+// SECTIONS-NEXT: .tbss.thread_zero128 NOBITS 00000000 {{[0-9a-f]+}} 000080 00 WAT 0 0 16
+// SECTIONS-NEXT: .tbss.thread_zero4096 NOBITS 00000000 {{[0-9a-f]+}} 001000 00 WAT 0 0 512
+// SECTIONS-NEXT: .tbss.thread_zero65535 NOBITS 00000000 {{[0-9a-f]+}} 010000 00 WAT 0 0 8192
+// SECTIONS-NEXT: .tbss.thread_zero65537 NOBITS 00000000 {{[0-9a-f]+}} 012000 00 WAT 0 0 8192
+// SECTIONS-NEXT: .tbss.thread_zero139267 NOBITS 00000000 {{[0-9a-f]+}} 024000 00 WAT 0 0 16384
+
+// Check that the sizes of the symbols have not been rounded up:
+// SECTIONS-LABEL: Symbol table '.symtab'
+// SECTIONS: 128 OBJECT LOCAL DEFAULT {{.*}} char128
+// SECTIONS: 4096 OBJECT LOCAL DEFAULT {{.*}} char4096
+// SECTIONS: 128 TLS LOCAL DEFAULT {{.*}} thread_char128
+// SECTIONS: 4096 TLS LOCAL DEFAULT {{.*}} thread_char4096
+// SECTIONS: 128 OBJECT LOCAL DEFAULT {{.*}} zero128
+// SECTIONS: 4096 OBJECT LOCAL DEFAULT {{.*}} zero4096
+// SECTIONS: 65535 OBJECT LOCAL DEFAULT {{.*}} zero65535
+// SECTIONS: 65537 OBJECT LOCAL DEFAULT {{.*}} zero65537
+// SECTIONS: 139267 OBJECT LOCAL DEFAULT {{.*}} zero139267
+// SECTIONS: 65535 OBJECT GLOBAL DEFAULT {{.*}} char65535
+// SECTIONS: 65537 OBJECT GLOBAL HIDDEN {{.*}} char65537
+// SECTIONS: 139267 OBJECT GLOBAL PROTECTED {{.*}} char139267
+// SECTIONS: 65535 TLS GLOBAL DEFAULT {{.*}} thread_char65535
+// SECTIONS: 65537 TLS GLOBAL HIDDEN {{.*}} thread_char65537
+// SECTIONS: 139267 TLS GLOBAL PROTECTED {{.*}} thread_char139267
+// SECTIONS: 00000010 128 OBJECT GLOBAL DEFAULT COM common128
+// SECTIONS: 00000200 4096 OBJECT GLOBAL DEFAULT COM common4096
+// SECTIONS: 00002000 65535 OBJECT GLOBAL DEFAULT COM common65535
+// SECTIONS: 00002000 65537 OBJECT GLOBAL HIDDEN COM common65537
+// SECTIONS: 00004000 139267 OBJECT GLOBAL PROTECTED COM common139267
+// SECTIONS: 128 TLS GLOBAL DEFAULT {{.*}} thread_zero128
+// SECTIONS: 4096 TLS GLOBAL DEFAULT {{.*}} thread_zero4096
+// SECTIONS: 65535 TLS GLOBAL DEFAULT {{.*}} thread_zero65535
+// SECTIONS: 65537 TLS GLOBAL HIDDEN {{.*}} thread_zero65537
+// SECTIONS: 139267 TLS GLOBAL PROTECTED {{.*}} thread_zero139267
+
+__attribute__((used)) static char char128[128] = {1};
+__attribute__((used)) static char char4096[4096] = {2};
+char char65535[65535] = {3};
+__attribute__((visibility("hidden"))) char char65537[65537] = {4};
+__attribute__((visibility("protected"))) char char139267[139267] = {5};
+
+// In the IR all variables should have align 1
+// IR: @char128 = internal addrspace(200) global <{ i8, [127 x i8] }> <{ i8 1, [127 x i8] zeroinitializer }>, align 1
+// IR: @char4096 = internal addrspace(200) global <{ i8, [4095 x i8] }> <{ i8 2, [4095 x i8] zeroinitializer }>, align 1
+// IR: @char65535 = dso_local addrspace(200) global <{ i8, [65534 x i8] }> <{ i8 3, [65534 x i8] zeroinitializer }>, align 1
+// IR: @char65537 = hidden addrspace(200) global <{ i8, [65536 x i8] }> <{ i8 4, [65536 x i8] zeroinitializer }>, align 1
+// IR: @char139267 = protected addrspace(200) global <{ i8, [139266 x i8] }> <{ i8 5, [139266 x i8] zeroinitializer }>, align 1
+
+// TLS variables:
+__attribute__((used)) __thread static char thread_char128[128] = {1};
+__attribute__((used)) __thread static char thread_char4096[4096] = {2};
+__thread char thread_char65535[65535] = {3};
+__attribute__((visibility("hidden"))) __thread char thread_char65537[65537] = {4};
+__attribute__((visibility("protected"))) __thread char thread_char139267[139267] = {5};
+// IR: @thread_char128 = internal thread_local addrspace(200) global <{ i8, [127 x i8] }> <{ i8 1, [127 x i8] zeroinitializer }>, align 1
+// IR: @thread_char4096 = internal thread_local addrspace(200) global <{ i8, [4095 x i8] }> <{ i8 2, [4095 x i8] zeroinitializer }>, align 1
+// IR: @thread_char65535 = dso_local thread_local addrspace(200) global <{ i8, [65534 x i8] }> <{ i8 3, [65534 x i8] zeroinitializer }>, align 1
+// IR: @thread_char65537 = hidden thread_local addrspace(200) global <{ i8, [65536 x i8] }> <{ i8 4, [65536 x i8] zeroinitializer }>, align 1
+// IR: @thread_char139267 = protected thread_local addrspace(200) global <{ i8, [139266 x i8] }> <{ i8 5, [139266 x i8] zeroinitializer }>, align 1
+
+
+// Also test bss variables
+__attribute__((used)) static char zero128[128];
+__attribute__((used)) static char zero4096[4096];
+__attribute__((used)) static char zero65535[65535];
+__attribute__((used, visibility("hidden"))) static char zero65537[65537];
+__attribute__((used, visibility("protected"))) static char zero139267[139267];
+// IR: @zero128 = internal addrspace(200) global [128 x i8] zeroinitializer, align 1
+// IR: @zero4096 = internal addrspace(200) global [4096 x i8] zeroinitializer, align 1
+// IR: @zero65535 = internal addrspace(200) global [65535 x i8] zeroinitializer, align 1
+// IR: @zero65537 = internal addrspace(200) global [65537 x i8] zeroinitializer, align 1
+// IR: @zero139267 = internal addrspace(200) global [139267 x i8] zeroinitializer, align 1
+
+// Common symbols:
+char common128[128];
+char common4096[4096];
+char common65535[65535];
+__attribute__((visibility("hidden"))) char common65537[65537];
+__attribute__((visibility("protected"))) char common139267[139267];
+
+// IR: @common128 = dso_local addrspace(200) global [128 x i8] zeroinitializer, align 1
+// IR: @common4096 = dso_local addrspace(200) global [4096 x i8] zeroinitializer, align 1
+// IR: @common65535 = dso_local addrspace(200) global [65535 x i8] zeroinitializer, align 1
+// IR: @common65537 = hidden addrspace(200) global [65537 x i8] zeroinitializer, align 1
+// IR: @common139267 = protected addrspace(200) global [139267 x i8] zeroinitializer, align 1
+
+// zero-init TLS
+__attribute__((used)) __thread char thread_zero128[128];
+__attribute__((used)) __thread char thread_zero4096[4096];
+__thread char thread_zero65535[65535];
+__attribute__((visibility("hidden"))) __thread char thread_zero65537[65537];
+__attribute__((visibility("protected"))) __thread char thread_zero139267[139267];
+// IR: @thread_zero128 = dso_local thread_local addrspace(200) global [128 x i8] zeroinitializer, align 1
+// IR: @thread_zero4096 = dso_local thread_local addrspace(200) global [4096 x i8] zeroinitializer, align 1
+// IR: @thread_zero65535 = dso_local thread_local addrspace(200) global [65535 x i8] zeroinitializer, align 1
+// IR: @thread_zero65537 = hidden thread_local addrspace(200) global [65537 x i8] zeroinitializer, align 1
+// IR: @thread_zero139267 = protected thread_local addrspace(200) global [139267 x i8] zeroinitializer, align 1
+
+// ASM-LABEL: .type char128,@object
+// ASM-NEXT: .section .data.char128,"aw",@progbits
+// ASM-NEXT: .p2align 4, 0x0
+// ASM-NEXT: char128:
+// ASM-NEXT: .byte 1
+// ASM-NEXT: .zero 127
+// ASM-NEXT: .size char128, 128
+// No need for tail padding here but need to align section:
+// ASM-LABEL: .type char4096,@object
+// ASM-NEXT: .section .data.char4096,"aw",@progbits
+// ASM-NEXT: .p2align 9, 0x0
+// ASM-NEXT: char4096:
+// ASM-NEXT: .byte 2
+// ASM-NEXT: .zero 4095
+// ASM-NEXT: .size char4096, 4096
+// ASM-LABEL: .type char65535,@object
+// ASM-NEXT: .section .data.char65535,"aw",@progbits
+// ASM-NEXT: .globl char65535
+// ASM-NEXT: .p2align 13, 0x0
+// ASM-NEXT: char65535:
+// ASM-NEXT: .byte 3
+// ASM-NEXT: .zero 65534
+// ASM-NEXT: .zero 1 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size char65535, 65535
+// ASM-LABEL: .hidden char65537
+// ASM-LABEL: .type char65537,@object
+// ASM-NEXT: .section .data.char65537,"aw",@progbits
+// ASM-NEXT: .globl char65537
+// ASM-NEXT: .p2align 13, 0x0
+// ASM-NEXT: char65537:
+// ASM-NEXT: .byte 4
+// ASM-NEXT: .zero 65536
+// ASM-NEXT: .zero 8191 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size char65537, 65537
+// ASM-LABEL: .protected char139267
+// ASM-LABEL: .type char139267,@object
+// ASM-NEXT: .section .data.char139267,"aw",@progbits
+// ASM-NEXT: .globl char139267
+// ASM-NEXT: .p2align 14, 0x0
+// ASM-NEXT: char139267:
+// ASM-NEXT: .byte 5
+// ASM-NEXT: .zero 139266
+// ASM-NEXT: .zero 8189 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size char139267, 139267
+// ASM-LABEL: .type thread_char128,@object
+// ASM-NEXT: .section .tdata.thread_char128,"awT",@progbits
+// ASM-NEXT: .p2align 4, 0x0
+// ASM-NEXT: thread_char128:
+// ASM-NEXT: .byte 1
+// ASM-NEXT: .zero 127
+// ASM-NEXT: .size thread_char128, 128
+// ASM-LABEL: .type thread_char4096,@object
+// ASM-NEXT: .section .tdata.thread_char4096,"awT",@progbits
+// ASM-NEXT: .p2align 9, 0x0
+// ASM-NEXT: thread_char4096:
+// ASM-NEXT: .byte 2
+// ASM-NEXT: .zero 4095
+// ASM-NEXT: .size thread_char4096, 4096
+// ASM-LABEL: .type thread_char65535,@object
+// ASM-NEXT: .section .tdata.thread_char65535,"awT",@progbits
+// ASM-NEXT: .globl thread_char65535
+// ASM-NEXT: .p2align 13, 0x0
+// ASM-NEXT: thread_char65535:
+// ASM-NEXT: .byte 3
+// ASM-NEXT: .zero 65534
+// ASM-NEXT: .zero 1 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size thread_char65535, 65535
+// ASM-LABEL: .hidden thread_char65537
+// ASM-LABEL: .type thread_char65537,@object
+// ASM-NEXT: .section .tdata.thread_char65537,"awT",@progbits
+// ASM-NEXT: .globl thread_char65537
+// ASM-NEXT: .p2align 13, 0x0
+// ASM-NEXT: thread_char65537:
+// ASM-NEXT: .byte 4
+// ASM-NEXT: .zero 65536
+// ASM-NEXT: .zero 8191 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size thread_char65537, 65537
+// ASM-LABEL: .protected thread_char139267
+// ASM-LABEL: .type thread_char139267,@object
+// ASM-NEXT: .section .tdata.thread_char139267,"awT",@progbits
+// ASM-NEXT: .globl thread_char139267
+// ASM-NEXT: .p2align 14, 0x0
+// ASM-NEXT: thread_char139267:
+// ASM-NEXT: .byte 5
+// ASM-NEXT: .zero 139266
+// ASM-NEXT: .zero 8189 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size thread_char139267, 139267
+// ASM-LABEL: .type zero128,@object
+// ASM-NEXT: .section .bss.zero128,"aw",@nobits
+// ASM-NEXT: .p2align 4, 0x0
+// ASM-NEXT: zero128:
+// ASM-NEXT: .zero 128
+// ASM-NEXT: .size zero128, 128
+// ASM-LABEL: .type zero4096,@object
+// ASM-NEXT: .section .bss.zero4096,"aw",@nobits
+// ASM-NEXT: .p2align 9, 0x0
+// ASM-NEXT: zero4096:
+// ASM-NEXT: .zero 4096
+// ASM-NEXT: .size zero4096, 4096
+// ASM-LABEL: .type zero65535,@object
+// ASM-NEXT: .section .bss.zero65535,"aw",@nobits
+// ASM-NEXT: .p2align 13, 0x0
+// ASM-NEXT: zero65535:
+// ASM-NEXT: .zero 65535
+// ASM-NEXT: .zero 1 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size zero65535, 65535
+// ASM-LABEL: .type zero65537,@object
+// ASM-NEXT: .section .bss.zero65537,"aw",@nobits
+// ASM-NEXT: .p2align 13, 0x0
+// ASM-NEXT: zero65537:
+// ASM-NEXT: .zero 65537
+// ASM-NEXT: .zero 8191 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size zero65537, 65537
+// ASM-LABEL: .type zero139267,@object
+// ASM-NEXT: .section .bss.zero139267,"aw",@nobits
+// ASM-NEXT: .p2align 14, 0x0
+// ASM-NEXT: zero139267:
+// ASM-NEXT: .zero 139267
+// ASM-NEXT: .zero 8189 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size zero139267, 139267
+// ASM-LABEL: .type common128,@object
+// ASM-NEXT: .comm common128,128,16
+// ASM-LABEL: .type common4096,@object
+// ASM-NEXT: .comm common4096,4096,512
+// ASM-LABEL: .type common65535,@object
+// ASM-NEXT: .comm common65535,65536,8192 # adding 1 bytes of tail padding for precise bounds.
+// ASM-NEXT: .size common65535, 65535 # explicit size directive required due to 1 bytes of tail padding for precise bounds.
+// ASM-LABEL: .hidden common65537
+// ASM-LABEL: .type common65537,@object
+// ASM-NEXT: .comm common65537,73728,8192 # adding 8191 bytes of tail padding for precise bounds.
+// ASM-NEXT: .size common65537, 65537 # explicit size directive required due to 8191 bytes of tail padding for precise bounds.
+// ASM-LABEL: .protected common139267
+// ASM-LABEL: .type common139267,@object
+// ASM-NEXT: .comm common139267,147456,16384 # adding 8189 bytes of tail padding for precise bounds.
+// ASM-NEXT: .size common139267, 139267 # explicit size directive required due to 8189 bytes of tail padding for precise bounds.
+// ASM-LABEL: .type thread_zero128,@object
+// ASM-NEXT: .section .tbss.thread_zero128,"awT",@nobits
+// ASM-NEXT: .globl thread_zero128
+// ASM-NEXT: .p2align 4, 0x0
+// ASM-NEXT: thread_zero128:
+// ASM-NEXT: .zero 128
+// ASM-NEXT: .size thread_zero128, 128
+// ASM-LABEL: .type thread_zero4096,@object
+// ASM-NEXT: .section .tbss.thread_zero4096,"awT",@nobits
+// ASM-NEXT: .globl thread_zero4096
+// ASM-NEXT: .p2align 9, 0x0
+// ASM-NEXT: thread_zero4096:
+// ASM-NEXT: .zero 4096
+// ASM-NEXT: .size thread_zero4096, 4096
+// ASM-LABEL: .type thread_zero65535,@object
+// ASM-NEXT: .section .tbss.thread_zero65535,"awT",@nobits
+// ASM-NEXT: .globl thread_zero65535
+// ASM-NEXT: .p2align 13, 0x0
+// ASM-NEXT: thread_zero65535:
+// ASM-NEXT: .zero 65535
+// ASM-NEXT: .zero 1 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size thread_zero65535, 65535
+// ASM-LABEL: .hidden thread_zero65537
+// ASM-LABEL: .type thread_zero65537,@object
+// ASM-NEXT: .section .tbss.thread_zero65537,"awT",@nobits
+// ASM-NEXT: .globl thread_zero65537
+// ASM-NEXT: .p2align 13, 0x0
+// ASM-NEXT: thread_zero65537:
+// ASM-NEXT: .zero 65537
+// ASM-NEXT: .zero 8191 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size thread_zero65537, 65537
+// ASM-LABEL: .protected thread_zero139267
+// ASM-LABEL: .type thread_zero139267,@object
+// ASM-NEXT: .section .tbss.thread_zero139267,"awT",@nobits
+// ASM-NEXT: .globl thread_zero139267
+// ASM-NEXT: .p2align 14, 0x0
+// ASM-NEXT: thread_zero139267:
+// ASM-NEXT: .zero 139267
+// ASM-NEXT: .zero 8189 # Tail padding to ensure precise bounds
+// ASM-NEXT: .size thread_zero139267, 139267
+
